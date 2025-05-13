9715280b2995b738cb2c3be399bfc287f5030751
rismamutia <rismamutiadewi89@gmail.com>
2025-05-13 19:03:08 +0700

    pembuatan class mahasiswa beserta member dan akses modifiernya

---
+++ constructoroverloading.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+    private:
+    int nim;
+    string nama;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
