98492fb69e05c695cff25d2cf7a741a4d13057c4
rismamutia <rismamutiadewi89@gmail.com>
2025-05-13 19:18:06 +0700

    menampilkan output constructor overloading

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -23,3 +23,26 @@ mahasiswa :: mahasiswa(int iNim){
 mahasiswa :: mahasiswa(string iNama){
     nama = iNama; //definisi hanya NAMA
 }
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; //definisi NIM dan NAMA
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+    cout << endl << "Nim = " <<nim << endl;
+    cout <<  "Nama = " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs1(102030);          //hanya NIM
+    mahasiswa mhs2("Andi");          //hanya nama
+    mahasiswa mhs3(010203, "Santi"); //NIM dan Nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1: "; mhs1.cetak();
+    cout << "Mahasiswa 2: "; mhs2.cetak();
+    cout << "Mahasiswa 3: "; mhs3.cetak();
+
+    return 0;
+}
