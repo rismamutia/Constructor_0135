d1953d253f04606d31eb285bd9a075cb956a0813
rismamutia <rismamutiadewi89@gmail.com>
2025-05-13 18:35:55 +0700

    mendeklarasikan konstruktor baru

---
+++ soal.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+class bangunDatar;
+
+class PersegiPanjang
+{
+    public:
+        void inputData(BangunDatar &bd);
+        void outputData(BangunDatar &bd);
+};
+
+class BangunDatar
+{
+    private:
+        float panjang;
+        float lebar;
+        float hitungLuas(){
+            return panjang * lebar;
+        };
+        float hitungKeliling(){
+            return 2 * (panjang + lebar);
+        };
+    public:
+        friend void PersegiPanjang::inputData(BangunDatar &bd);
+        friend void PersegiPanjang::outputData(BangunDatar &bd);
+};
+
+void PersegiPanjang::inputData(BangunDatar &bd)
+{
+    cout << "Masukkan panjang : ";
+    cin >> bd.panjang;
+    cout << "Masukkan lebar : ";
+    cin >> bd.lebar;
+}
+
+void PersegiPanjang::outputData(BangunDatar &bd)
+{
+    cout << "Luas : " << bd.hitungLuas() << endl;
+    cout << "Keliling : " << bd.hitungKeliling() << endl;
+};
+        
+
+int main()
+{
+   PersegiPanjang pP;
+   BangunDatar bD;
+   pP.inputData(bD);
+   pP.outputData(bD); 
+};
